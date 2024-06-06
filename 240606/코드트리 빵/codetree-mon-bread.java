1ff5b0da9cdfb31031c44e79e65849e8309b3682
JinSeong-Bang <74286424+wlstjdwkd@users.noreply.github.com>
2024-06-06 21:37:55 +0900

    Title: 코드트리 빵, Time: 112ms, Memory: 9MB, Status: Passed - CodeTree

--- 240606/코드트리 빵/codetree-mon-bread.java
+++ 240606/코드트리 빵/codetree-mon-bread.java
@@ -1,94 +1,214 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M;
-	static int[][] map;
-	static int[][] store;
-	static boolean[][] v;
-	static ArrayDeque<int[]> people;
-	static int[] dx = {-1, 0, 0, 1}, dy = {0, -1, 1, 0};
-    public static void main(String args[]) throws Exception{
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        map = new int[N][N];
-        for(int i=0; i<N; i++) {
-        	st = new StringTokenizer(bf.readLine(), " ");
-        	for(int j=0; j<N; j++)
-        		map[i][j] = Integer.parseInt(st.nextToken());
-        }
-        store = new int[M][2];
-        v = new boolean[N][N];
-        for(int i=0; i<M; i++){
-        	st = new StringTokenizer(bf.readLine(), " ");
-        	store[i][0] = Integer.parseInt(st.nextToken())-1;
-        	store[i][1] = Integer.parseInt(st.nextToken())-1;
-        }
-        people = new ArrayDeque<>();
-        int cnt = 0;
-        while(true) {
-        	int size = people.size();
-        	if(size == 0 && cnt!=0) break;
-        	while(size-- > 0) {
-        		int[] t = people.poll();
-        		int[] add = bfs(t[0], t[1], t[2]);
-        		if(add[1] == store[t[0]][0] && add[2] == store[t[0]][1]) // 편의점 도착
-        			v[add[1]][add[2]] = true;
-        		else people.offer(add);
-        	}
-        	if(cnt < M)
-        		people.offer(find_basecamp(cnt, store[cnt][0], store[cnt][1]));
-        	cnt++;
-        }
-        System.out.println(cnt);
-    }
-    static int[] bfs(int n, int x, int y) {
-    	ArrayDeque<int[]> q = new ArrayDeque<>();
-    	q.offer(new int[] {x, y, -1, -1});
-    	boolean[][] visited = new boolean[N][N];
-    	visited[x][y] = true;
-    	while(!q.isEmpty()) {
-    		int[] now = q.poll();
-    		if(now[0] == store[n][0] && now[1] == store[n][1]) {
-    			return new int[] {n, now[2], now[3]};
-    		}
-    		for(int d=0; d<4; d++) {
-    			int nx = now[0] + dx[d];
-    			int ny = now[1] + dy[d];
-    			if(nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny] || v[nx][ny]) continue;
-    			visited[nx][ny] = true;
-    			if(now[2] == -1) {
-    				q.offer(new int[] {nx, ny, nx, ny});
-    			}
-    			else
-    				q.offer(new int[] {nx, ny, now[2], now[3]});
-    		}
-     	}
-    	return null; // number+이동한 곳의 좌표를 리턴!
-    }
-    static int[] find_basecamp(int n, int x, int y) { // @param : 편의점의 위치 - 가장 가까운 base camp 찾기
-//    	System.out.println(x+", "+y);
-    	ArrayDeque<int[]> q = new ArrayDeque<>();
-    	q.offer(new int[] {x, y});
-    	boolean[][] visited = new boolean[N][N];
-    	visited[x][y] = true;
-    	while(!q.isEmpty()) {
-    		int[] now = q.poll();
-    		
-    		if(map[now[0]][now[1]] == 1) {
-    			v[now[0]][now[1]] = true;
-    			return new int[] {n, now[0], now[1]};
-    		}
-    		for(int d=0; d<4; d++) {
-    			int nx = now[0] + dx[d];
-    			int ny = now[1] + dy[d];
-    			if(nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny] || v[nx][ny]) continue;
-    			visited[nx][ny] = true;
-    			q.offer(new int[] {nx, ny});
-    		}
-     	}
-    	return null; // number+해당 베이스캠프의 좌표를 리턴!
-    }
+	
+	static class Node implements Comparable<Node> {
+		int x;
+		int y;
+		int dir;
+		int dist;
+		
+		public Node(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		public Node(int x, int y, int dist) {
+			this.x = x;
+			this.y = y;
+			this.dist = dist;
+		}
+		
+		public Node(int x, int y, int dir, int dist) {
+			this.x = x;
+			this.y = y;
+			this.dir = dir;
+			this.dist = dist;
+		}
+		
+		public boolean isSame(Node node) {
+			return this.x == node.x && this.y == node.y;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			if (this.dist == o.dist) {
+				if (this.x == o.x) {
+					return this.y - o.y;
+				}
+				
+				return this.x - o.x;
+			}
+			
+			return this.dist - o.dist;
+		}
+	}
+	
+	// 상좌우하
+	static int[] dx = {-1, 0, 0, 1};
+	static int[] dy = {0, -1, 1, 0};
+	
+	static int n, m;
+	static int[][] map; // 0 빈 공간, 1 베이스캠프, 2 지나갈 수 없는 곳
+	
+	static Node[] person;
+	static Node[] store;
+	
+	static int time;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		map = new int[n][n];
+		person = new Node[m];
+		store = new Node[m];
+		
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			for (int j = 0; j < n; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int x = Integer.parseInt(st.nextToken()) - 1;
+			int y = Integer.parseInt(st.nextToken()) - 1;
+			
+			store[i] = new Node(x, y);
+			person[i] = new Node(-1, -1);
+		}
+		
+		time = 1;
+		
+		while (true) {
+			moveStore();
+			
+			if (time <= m) {
+				moveBasecamp(store[time - 1]);	
+			}
+			
+			if (isFinish()) {
+				break;
+			}
+			
+			time++;
+		}
+		
+		System.out.println(time);
+	}
+	
+	private static void moveStore() {
+		for (int i = 0; i < m; i++) {
+			Node start = person[i];
+			Node end = store[i];
+			
+			// 격자 밖에 있거나 편의점에 도착한 경우 다음으로 넘어감
+			if (!isRange(start.x, start.y) || start.isSame(end)) {
+				continue;
+			}
+			
+			int dir = findDir(start, end);
+			
+			start.x += dx[dir];
+			start.y += dy[dir];
+		}
+		
+		// 모두 움직인 후에 편의점에 도착했는지 확인
+		for (int i = 0; i < m; i++) {
+			if (person[i].isSame(store[i])) { 
+				map[person[i].x][person[i].y] = 2; // 지나갈 수 없는 곳으로 변경
+			}
+		}
+	}
+	
+	// 가고싶은 편의점까지 최단거리가 되는 방향을 구함
+	private static int findDir(Node start, Node end) {
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		boolean[][] visited = new boolean[n][n];
+		
+		pq.add(new Node(start.x, start.y, -1, 0));
+		visited[start.x][start.y] = true;
+		
+		while (!pq.isEmpty()) {
+			Node cur = pq.poll();
+			
+			// 편의점으로 가는 최단거리가 여러 개가 될수도 있음
+			if (cur.isSame(end)) {
+				return cur.dir;
+			}
+			
+			for (int i = 0; i < 4; i++) {
+				int nx = cur.x + dx[i];
+				int ny = cur.y + dy[i];
+				
+				if (!isRange(nx, ny) || visited[nx][ny] || map[nx][ny] == 2) {
+					continue;
+				}
+				
+				pq.add(new Node(nx, ny, cur.dir == -1 ? i : cur.dir, cur.dist + 1));
+				visited[nx][ny] = true;
+			}
+		}
+		
+		return 0;
+	}
+	
+	private static void moveBasecamp(Node start) {
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		boolean[][] visited = new boolean[n][n];
+		
+		pq.add(new Node(start.x, start.y, 0));
+		visited[start.x][start.y] = true;
+		
+		while (!pq.isEmpty()) {
+			Node cur = pq.poll();
+			
+			// 최단거리가 같은 베이스캠프가 여러 개 있을 수도 있음
+			if (map[cur.x][cur.y] == 1) {
+				map[cur.x][cur.y] = 2;
+				person[time - 1] = cur;
+				
+				return;
+			}
+			
+			for (int i = 0; i < 4; i++) {
+				int nx = cur.x + dx[i];
+				int ny = cur.y + dy[i];
+				
+				if (!isRange(nx, ny) || visited[nx][ny] || map[nx][ny] == 2) {
+					continue;
+				}
+				
+				pq.add(new Node(nx, ny, cur.dist + 1));
+				visited[nx][ny] = true;
+			}
+		}
+	}
+	
+	// 모든 사람이 편의점에 도착했는지 확인
+	private static boolean isFinish() {
+		for (int i = 0; i < m; i++) {
+			if (!person[i].isSame(store[i])) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	private static boolean isRange(int x, int y) {
+		return x >= 0 && x < n && y >= 0 && y < n;
+	}
+
 }
